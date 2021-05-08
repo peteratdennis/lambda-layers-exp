cc32fab8199bd6f68fe4a90e3fc18e4a4ae1f94e
peteratdennis <peter_clarke@dennis.co.uk>
2021-05-08 19:13:44 +0100

    No ES6 for now

--- src/libs/CapitaliseES6.mjs
+++
@@ -1,6 +0,0 @@
-export const capitaliseFirstLetter = string => string.charAt(0).toUpperCase()
-  + string.slice(1).toLowerCase();
-
-export default {
-  capitaliseFirstLetter,
-};
--- src/libs/Hello.js
+++ src/libs/Hello.js
@@ -1,7 +1,8 @@
-
-const { capitaliseFirstLetter } = require('../libs/Capitalise');
+const Logger = require('./Logger');
+const { capitaliseFirstLetter } = require('./Capitalise');
 
 const handler = (event) => {
+  Logger.debug('Event', event);
   const message = capitaliseFirstLetter('hello world!');
   return {
     message,
